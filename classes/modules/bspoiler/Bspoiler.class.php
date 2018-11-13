bf59017a6cce9acc820419f9cd474faddf89a04b
Karel Wintersky <karel.wintersky@yandex.ru>
2018-11-13 08:19:22 +0300

    added `hook_priority` config mechanics

--- classes/modules/bspoiler/Bspoiler.class.php
+++ classes/modules/bspoiler/Bspoiler.class.php
@@ -1,19 +1,5 @@
 <?php
-
 /*-------------------------------------------------------
-*
-*   LiveStreet Engine Social Networking
-*   Copyright © 2008 Mzhelskiy Maxim
-*
-*--------------------------------------------------------
-*
-*   Official site: www.livestreet.ru
-*   Contact e-mail: rus.engine@gmail.com
-*
-*   GNU General Public License, version 2:
-*   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-*
-*---------------------------------------------------------
 *
 *	Plugin Spoiler
 *	Shpinev Konstantin
--- config/config.php
+++ config/config.php
@@ -2,19 +2,6 @@
 
 /*-------------------------------------------------------
 *
-*   LiveStreet Engine Social Networking
-*   Copyright © 2008 Mzhelskiy Maxim
-*
-*--------------------------------------------------------
-*
-*   Official site: www.livestreet.ru
-*   Contact e-mail: rus.engine@gmail.com
-*
-*   GNU General Public License, version 2:
-*   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-*
-*---------------------------------------------------------
-*
 *	Plugin Spoiler
 *	Shpinev Konstantin
 *	Contact e-mail: thedoublekey@gmail.com
